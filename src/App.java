a8c3ef45b0e6f59a36d481592e3f39aaedf565d1
Tomás Varas Oviedo <88008465+TomyVarasOviedo@users.noreply.github.com>
2023-06-14 21:22:02 -0300

    insertar

--- src/App.java
+++ src/App.java
@@ -3,6 +3,6 @@ import db.BaseDatos;
 public class App {
     public static void main(String[] args) throws Exception {
         BaseDatos dBaseDatos = new BaseDatos();
-        dBaseDatos.obtenerClientes();
+        dBaseDatos.obtenerStock();
     }
 }
